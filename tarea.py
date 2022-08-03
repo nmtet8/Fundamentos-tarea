7a49e4dfe910f79965b117930f1ca6e1c221b7b9
nmtet8 <vivanchi@gmail.com>
2022-08-03 18:27:00 -0300

    EJercicio tema 8

--- tarea.py
+++ tarea.py
@@ -1,19 +1,31 @@
-def suma(a, b, c):
-    print(a + b + c)
+numeroIf = input("Ingrese un número ")
 
+if int(numeroIf) < 0:
+    print("El número que hes ingresado es negativo")
+elif int(numeroIf) > 0:
+    print("El número que has ingresado es negativo")
+else:
+    print("Has marcado el cero")
 
-class Coche(object):
+numeroWhile = 0
 
-    def __init__(self, puertas=0):
+while int(numeroWhile) < 3:
+    print(numeroWhile)
+    numeroWhile += 1
 
-        self.puertas = puertas
+numeroFor = 3
 
-    def aumentar(self):
-        self.puertas += 1
+for x in range(numeroFor):
+    numeroFor -= 1
+    print(numeroFor)
 
+estacion = "VERANO"
 
-miCoche = Coche(4)
-print(miCoche.puertas)
-
-miCoche.aumentar()
-print(miCoche.puertas)
+if estacion == "INVIERNO":
+    print(" es invierno")
+elif estacion == "OTOÑO":
+    print(" es otoño")
+elif estacion == "PRIMAVERA":
+    print(" es primavera")
+else:
+    print("es verano")
